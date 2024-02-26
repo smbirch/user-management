df9b21adfff5f59bbdfb858a8fa14ef1b37fd1b3
Austen Medina <austen@Austens-MBP.attlocal.net>
2024-02-26 17:53:52 -0600

    added link button component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { HomeComponent } from './components/home/home.component';
+import { LinkButtonComponent } from './components/link-button/link-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CompaniesComponent,
+    HomeComponent,
+    LinkButtonComponent
   ],
   imports: [
     BrowserModule,
