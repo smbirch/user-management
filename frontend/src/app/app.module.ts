bfca995fb3b32ad980916fd5b3ba976a5ffe60df
smbirch <spencer.m.birch@gmail.com>
2024-02-26 18:39:25 -0800

    created components for all neccesary

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,33 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { CompanyComponent } from './company/company.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UsersRegistryComponent } from './users-registry/users-registry.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CreateProjectModalComponent } from './create-project-modal/create-project-modal.component';
+import { CreateTeamModalComponent } from './create-team-modal/create-team-modal.component';
+import { CreateAnnouncementModalComponent } from './create-announcement-modal/create-announcement-modal.component';
+import { EditProjectModalComponent } from './edit-project-modal/edit-project-modal.component';
+import { AddUserModalComponent } from './add-user-modal/add-user-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CompanyComponent,
+    HomeComponent,
+    TeamsComponent,
+    ProjectsComponent,
+    UsersRegistryComponent,
+    NavbarComponent,
+    CreateProjectModalComponent,
+    CreateTeamModalComponent,
+    CreateAnnouncementModalComponent,
+    EditProjectModalComponent,
+    AddUserModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/create-announcement-modal/create-announcement-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-announcement-modal',
+  templateUrl: './create-announcement-modal.component.html',
+  styleUrls: ['./create-announcement-modal.component.css']
+})
+export class CreateAnnouncementModalComponent {
+
+}
