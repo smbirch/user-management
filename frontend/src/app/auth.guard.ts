2369dc986166195f1441aebda5ec53aed9a8adca
smbirch <spencer.m.birch@gmail.com>
2024-02-26 21:03:16 -0800

    implements menubar component with links to main pages and logout button

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (isLoggedIn) {
+    if (isLoggedIn === 'true') {
       return true;
     } else {
       this.router.navigate(['/login']);
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {LoginComponent} from "../login/login.component";
+import {NgIf} from "@angular/common";
+import {UserServiceService} from "../../../services/user-service.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  showModal: boolean = false;
+
+  constructor(private userService: UserServiceService, private router: Router) {}
+
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  logout() {
+    this.toggleModal();
+    localStorage.setItem('isLoggedIn', 'false');
+    this.router.navigate(['/login']);
+  }
 
 }
