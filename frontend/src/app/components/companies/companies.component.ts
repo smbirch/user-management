957628378be19af34065957caae616d04db6c421
smbirch <spencer.m.birch@gmail.com>
2024-02-26 21:43:52 -0800

    implements Company component and calls backend for array of company DTOs

--- frontend/src/app/components/companies/companies.component.ts
+++ frontend/src/app/components/companies/companies.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {CompanyServiceService} from "../../../services/company-service.service";
+import {Company} from "../../../models/company";
+
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrls: ['./companies.component.css']
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
 
-  constructor(private router: Router){}
+  companies: any[] = [];
+
+  constructor(private router: Router, private companyService: CompanyServiceService) {}
+
+  ngOnInit(): void {
+    this.loadCompanies(); // loads companies
+  }
+
+  loadCompanies() {
+    this.companyService.getCompanies().subscribe(
+      (data: Company[]) => {
+        this.companies = data;
+      },
+      (error) => {
+        console.error('Error fetching companies:', error);
+      }
+    );
+  }
+
 
   goHome(){
     this.router.navigateByUrl('/home');
