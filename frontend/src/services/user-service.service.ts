37c4c91b101c3754ad0381b4c021965a4ad788eb
smbirch <spencer.m.birch@gmail.com>
2024-02-28 21:04:30 -0800

    login page alerts you for incorrect username or password

--- frontend/src/services/user-service.service.ts
+++ frontend/src/services/user-service.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { User } from 'src/models/user';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {FullUserDto} from "../app/full-user-dto";
+import {Injectable} from '@angular/core';
+import {User} from 'src/models/user';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
+
 class UserRequestDto {
 }
 
@@ -15,31 +15,34 @@ export class UserServiceService {
 
   constructor(private http: HttpClient) {
     this.usersUrl = 'http://localhost:8080/users';
-   }
+  }
 
-
-   public findAll(): Observable<User[]> {
+  public findAll(): Observable<User[]> {
     return this.http.get<User[]>(this.usersUrl);
-   }
+  }
 
-   public save(user: UserRequestDto) {
-     console.log(user)
+  public save(user: UserRequestDto) {
+    console.log(user)
     return this.http.post<User>(this.usersUrl, user);
-   }
+  }
 
-    public login(credentials: CredentialsDto): Observable<User> {
-        return this.http.post<User>(`${this.usersUrl}/login`, credentials);
-    }
+  public login(credentials: CredentialsDto): Observable<User> {
+    // @ts-ignore
+    return this.http.post<User>(`${this.usersUrl}/login`, credentials).pipe(
+      catchError(error => {
+        return throwError(error);
+      })
+    );
+  }
 
 
-public findCompanyActiveUsers(): Observable<User[]>
-{
+  public findCompanyActiveUsers(): Observable<User[]> {
 
-  return this.http.get<User[]>('http://localhost:8080/company/6/active-users');
+    return this.http.get<User[]>('http://localhost:8080/company/6/active-users');
+  }
 }
-   //probably need to implement some error handling.
-}
+
 export class CredentialsDto {
-    username: string | undefined;
-    password: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
 }
