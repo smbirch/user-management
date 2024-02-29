20841bb771ccecdeef50210406aac1156e4b48fa
smbirch <spencer.m.birch@gmail.com>
2024-02-28 23:46:55 -0800

    can create new users

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,12 +1,6 @@
 import {Company} from "./company";
 import {Team} from "./team";
-
-class Profile {
-  firstName: string | undefined;
-  lastName: string | undefined;
-  email: string | undefined;
-  phone: string | undefined;
-}
+import {ProfileDto} from "../app/profile-dto";
 
 export class User {
   id: number | undefined;
@@ -22,5 +16,5 @@ export class User {
   companies: Company[] | undefined;
   teams: Team[] | undefined;
   isLoggedIn: boolean | undefined;
-  profile: Profile | undefined;
+  profile: ProfileDto | undefined;
 }
