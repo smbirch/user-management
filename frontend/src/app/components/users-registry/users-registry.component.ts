031cd6ce805ebaa86daac129b4c4ca7e591b9366
smbirch <spencer.m.birch@gmail.com>
2024-02-28 19:31:22 -0800

    users registry cannot be accessed by non admins

--- frontend/src/app/components/users-registry/users-registry.component.ts
+++ frontend/src/app/components/users-registry/users-registry.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FullUserDto} from "../../full-user-dto";
 import {CompanyServiceService} from "../../../services/company-service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-users-registry',
@@ -11,25 +12,54 @@ export class UsersRegistryComponent implements OnInit {
   users: FullUserDto[] = [];
   showModal: boolean = false;
 
-  constructor(private companyService: CompanyServiceService) { }
+  constructor(private companyService: CompanyServiceService, private router: Router) { }
 
   ngOnInit(): void {
-    const selectedCompanyString = localStorage.getItem('selectedCompany');
-    if (!selectedCompanyString) {
-      throw new Error('company data not found in local storage');
+    let companyId: number;
+
+    const currentUserString = localStorage.getItem('currentUser');
+    if (!currentUserString) {
+      throw new Error('currentUser data not found in local storage.');
     }
-    const selectedCompany = JSON.parse(selectedCompanyString);
-    const companyId: number = selectedCompany.id;
-    this.companyService.getAllTotalUsers(companyId).subscribe(
-      (data: FullUserDto[]) => {
-        this.users = data;
-        console.log(data)
-      },
-      (error: any) => {
-        console.error('Error fetching users:', error);
+    const currentUser = JSON.parse(currentUserString);
+    const isAdmin = currentUser.admin;
+
+    if (isAdmin) {
+      const selectedCompanyString = localStorage.getItem('selectedCompany');
+      if (!selectedCompanyString) {
+        throw new Error('company data not found in local storage');
       }
-    );
+      const selectedCompany = JSON.parse(selectedCompanyString);
+      companyId = selectedCompany.id;
+      this.companyService.getAllTotalUsers(companyId).subscribe(
+        (data: FullUserDto[]) => {
+          this.users = data;
+          console.log(data);
+        },
+        (error: any) => {
+          console.error('Error fetching users:', error);
+        }
+      );
+    } else {
+      const companiesString = localStorage.getItem('companies');
+      if (!companiesString) {
+        throw new Error('Companies data not found in local storage.');
+      }
+      const companies = JSON.parse(companiesString);
+      companyId = companies[0].id;
+      console.log(companyId)
+      this.companyService.getAllTotalUsers(companyId).subscribe(
+        (data: FullUserDto[]) => {
+          this.users = data;
+          this.router.navigate(['/home']);
+        },
+        (error: any) => {
+          console.error('Error fetching users:', error);
+        }
+      );
+    }
   }
+
 
   getUserTeam(user: any): string {
     if (user && user.teams) {
@@ -43,7 +73,6 @@ export class UsersRegistryComponent implements OnInit {
   }
 
   closeAddUserModal() {
-    // Toggle the showModal property to false to hide the modal
     this.showModal = false;
   }
 }
