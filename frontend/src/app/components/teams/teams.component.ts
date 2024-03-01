a0086bf69354052ad3d6e5b5924b71f0c2a62e29
amfuma <anthony.fumagalli88@gmail.com>
2024-03-01 06:13:46 -0600

    final push

--- frontend/src/app/components/teams/teams.component.ts
+++ frontend/src/app/components/teams/teams.component.ts
@@ -12,6 +12,7 @@ import { UserServiceService } from 'src/services/user-service.service';
 import { Location } from '@angular/common';
 
 import { map } from 'rxjs';
+import { ProjectService } from 'src/services/project.service';
 
 @Component({
   selector: 'app-teams',
@@ -42,47 +43,46 @@ export class TeamsComponent implements OnInit {
   currentLoggedIn?: boolean;
   companyId?: number;
 
- 
-  constructor(private teamService: TeamService, protected modalService: ModalsService, private userService: UserServiceService, private router: Router, private location: Location){}
-  ngOnInit(){
+  constructor(
+    private teamService: TeamService,
+    protected modalService: ModalsService,
+    private userService: UserServiceService,
+    private router: Router,
+    private location: Location
+  ) {}
+  ngOnInit() {
     this.initializeCurrentUser();
-    console.log('companyId',this.companyId);
+    console.log('companyId', this.companyId);
     let t = this.testService();
     this.getUsers();
-    
   }
 
-
   getWorkerTeams(teams: Team[]): Team[] {
-
-   let  tempTeam: Team[] = [];
-   for(let team of teams){
-    if(team.teammates){
-      const isUserInTeam = team.teammates.some(user => user.id === this.currentId);
-      if(isUserInTeam){
-        tempTeam.push(team);
+    let tempTeam: Team[] = [];
+    for (let team of teams) {
+      if (team.teammates) {
+        const isUserInTeam = team.teammates.some(
+          (user) => user.id === this.currentId
+        );
+        if (isUserInTeam) {
+          tempTeam.push(team);
+        }
       }
     }
-   }
     return tempTeam;
   }
 
-
-  initializeCurrentUser(){
+  initializeCurrentUser() {
     const currentUserString = localStorage.getItem('currentUser');
     let currentCompanyString = localStorage.getItem('selectedCompany');
     let currentCompanyWorkerString = localStorage.getItem('companies');
-    console.log('companies',currentCompanyWorkerString)
-   console.log('current user', currentUserString)
-   
-
-
+    console.log('companies', currentCompanyWorkerString);
+    console.log('current user', currentUserString);
 
     //this for an admin user
-    if(currentUserString && currentCompanyString){
+    if (currentUserString && currentCompanyString) {
       const userDetails = JSON.parse(currentUserString);
       const companyDetail = JSON.parse(currentCompanyString);
-      
 
       this.currentActive = userDetails.active;
       this.currentAdmin = userDetails.admin;
@@ -97,60 +97,57 @@ export class TeamsComponent implements OnInit {
       console.log('user is admin', this.currentAdmin);
     }
 
-   
     //this is for a standard user
-    if(currentUserString && currentCompanyWorkerString){
+    if (currentUserString && currentCompanyWorkerString) {
       console.log('this is a standard user');
       const userDetails = JSON.parse(currentUserString);
       const workerDetail = JSON.parse(currentCompanyWorkerString);
-      
 
       this.currentActive = userDetails.active;
       this.currentAdmin = userDetails.admin;
       this.currentFirstName = userDetails.firstName;
       this.currentId = userDetails.id;
-      this.currentLoggedIn =userDetails.isLoggedIn;
-      this.currentLastName = userDetails.lastName
+      this.currentLoggedIn = userDetails.isLoggedIn;
+      this.currentLastName = userDetails.lastName;
       this.currentStatus = userDetails.status;
       this.companyId = workerDetail[0].id;
 
       console.log('the company id is', this.companyId);
       console.log('user is admin', this.currentAdmin);
     }
-    
   }
-  getTeams(){
-   this.teamService.getTeamsByCompanyId(this.company_id).subscribe({
-    next: (data) => {
-      this.teams = data
-      console.log('teams',data)
-    },
-    error: (error) => console.error('There was a problem getting company teams', error.message)
-   });
-   console.log(this.teams);
-
+  getTeams() {
+    this.teamService.getTeamsByCompanyId(this.company_id).subscribe({
+      next: (data) => {
+        this.teams = data;
+        console.log('teams', data);
+      },
+      error: (error) =>
+        console.error(
+          'There was a problem getting company teams',
+          error.message
+        ),
+    });
+    console.log(this.teams);
   }
 
   //this is just a test method
-  testService(){
-    if(this.companyId){
+  testService() {
+    if (this.companyId) {
       console.log('getting the teams');
       this.teamService.getTestTeam(this.companyId).subscribe({
         next: (data) => {
           this.teams = this.conformToTeam(data);
           this.workerTeams = this.getWorkerTeams(this.conformToTeam(data));
 
-
           console.log('worker teams', this.workerTeams);
-          console.log('got the teams',this.teams);
-          
+          console.log('got the teams', this.teams);
         },
         error: (error) => {
           console.error('There was a problem getting company teams', error);
-        }
-       });
+        },
+      });
     }
-
   }
 
   getUsers() {
@@ -174,87 +171,85 @@ export class TeamsComponent implements OnInit {
       conformedTeam.teammates = team.teammates.map((teammate: any) => {
         const user = new User();
 
-            user.id = teammate.id;
-            user.firstName = teammate.profile.firstName; 
-            user.lastName = teammate.profile.lastName; 
-            user.email = teammate.profile.email;
-            user.phone = teammate.profile.phone;
-            user.active = teammate.active;
-            user.admin = teammate.admin;
-            user.status = teammate.status;
-            return user;
-        });
-        return conformedTeam;
-    });
-}
-private conformToUsers(data: any[]): User[]{
-  return data.map(user => {
-    const conformedUser = new User();
-
-    conformedUser.id = user.id;
-    conformedUser.firstName = user.profile.firstName;
-    conformedUser.lastName = user.profile.lastName;
-    conformedUser.email = user.profile.email;
-    conformedUser.phone = user.profile.phone;
-    conformedUser.active = user.active;
-    conformedUser.admin = user.admin;
-    conformedUser.status = user.status;
-
-    return conformedUser;
-  });
- 
-}
-
-createTeam(){
-  console.log(this.newTeamName);
-  console.log(this.newTeamDescription);
-  const TeamDto = {id: this.companyId, name: this.newTeamName, description: this.newTeamDescription,
-    teammates: this.selectedUsers?.map(user => ({
-      id: user.id,
-      profile: {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        phone: user.phone
-      },
-      admin: user.admin,
-      active: user.active,
-      status: user.status
-
-    })),
-    author: {
-      id: 23,
-      profile: {
-      firstName: 'Logan',
-      lastName: 'Roy',
-      email: 'lroy@email.com',
-      phone: '(555) 555-5555'
-    },
-      admin: true,
-      active: true
-    }
-  };
-
-  if(this.companyId){
-    this.teamService.saveTeam(this.companyId, TeamDto).subscribe({
-      next: (response) => {
-        console.log('Team saved successfully', response);
-        
-      },
-      error: (error) => {
-        console.error('Error saving team', error);
-      },
-      complete: () => console.log('Completed') 
-
-      
+        user.id = teammate.id;
+        user.firstName = teammate.profile.firstName;
+        user.lastName = teammate.profile.lastName;
+        user.email = teammate.profile.email;
+        user.phone = teammate.profile.phone;
+        user.active = teammate.active;
+        user.admin = teammate.admin;
+        user.status = teammate.status;
+        return user;
+      });
+      return conformedTeam;
     });
   }
-   //auto reload
-   this.location.replaceState('/teams');
-   window.location.reload();
-}
-  
-  openModal(){
+  private conformToUsers(data: any[]): User[] {
+    return data.map((user) => {
+      const conformedUser = new User();
+
+      conformedUser.id = user.id;
+      conformedUser.firstName = user.profile.firstName;
+      conformedUser.lastName = user.profile.lastName;
+      conformedUser.email = user.profile.email;
+      conformedUser.phone = user.profile.phone;
+      conformedUser.active = user.active;
+      conformedUser.admin = user.admin;
+      conformedUser.status = user.status;
+
+      return conformedUser;
+    });
+  }
+
+  createTeam() {
+    console.log(this.newTeamName);
+    console.log(this.newTeamDescription);
+    const TeamDto = {
+      id: this.companyId,
+      name: this.newTeamName,
+      description: this.newTeamDescription,
+      teammates: this.selectedUsers?.map((user) => ({
+        id: user.id,
+        profile: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          phone: user.phone,
+        },
+        admin: user.admin,
+        active: user.active,
+        status: user.status,
+      })),
+      author: {
+        id: 23,
+        profile: {
+          firstName: 'Logan',
+          lastName: 'Roy',
+          email: 'lroy@email.com',
+          phone: '(555) 555-5555',
+        },
+        admin: true,
+        active: true,
+      },
+    };
+
+    if (this.companyId) {
+      this.teamService.saveTeam(this.companyId, TeamDto).subscribe({
+        next: (response) => {
+          console.log('Team saved successfully', response);
+        },
+        error: (error) => {
+          console.error('Error saving team', error);
+        },
+        complete: () => console.log('Completed'),
+      });
+    }
+    //auto reload
+    this.location.replaceState('/teams');
+    window.location.reload();
+  }
+
+  openModal() {
     this.modalService.open('modal-add-team');
   }
 
@@ -273,7 +268,4 @@ createTeam(){
       console.error('Team ID is undefined. Cannot navigate to projects.');
     }
   }
-
-  
-  
 }
